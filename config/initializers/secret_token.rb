2e35a2b8e996c983bbba0454cc0556d2ea1e9827
Albert Español <albertesga@gmail.com>
2013-04-14 22:06:28 +0200

    Commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jopuc::Application.config.secret_token = '682debb9a7b7c008ebfa97a8f88d623f32e6ab4ced6527074b5bf604a267c3d48f4c7655e87d9ce8e43ff8cb788c0f5897f60c7c6a7d31125ce5840bac95beb4'
