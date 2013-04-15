945bc43e8c2258d3247e4bba0940ac96655d7420
Albert Español <albertesga@gmail.com>
2013-04-15 17:34:51 +0200

    Challenges v0.1

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -1,20 +1,24 @@
 class ChallengesController < ApplicationController
   before_filter :signed_in_user, only: [:create, :destroy]
 
+
   def index
   end
 
   def create
-  	@challenge = current_user.microposts.build(params[:challenge])
+  	@challenge = current_user.challenges.build(params[:challenge])
+    
     if @challenge.save
       flash[:success] = "Reto creado"
-      redirect_to root_url
+      redirect_to current_user
     else
-      render 'static_pages/home'
+      redirect_to current_user
     end
   end
 
   def destroy
+    Challenge.find(params[:id]).destroy
+    redirect_to user
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @challenges = @user.challenges
+    @challenge = current_user.challenges.build if signed_in?
   end
 
   def update
